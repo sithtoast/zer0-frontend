8052b8c236b20e1067385632f524680fce633469
William Holt <william@williams-mac-studio.local>
2024-04-26 15:30:53 -0400

    hashrouter again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
