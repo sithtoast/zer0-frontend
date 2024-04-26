773d8b1b08f1fa5c7601db19c3773d748c761a7b
William Holt <william@williams-mac-studio.local>
2024-04-25 22:20:43 -0400

    hash router!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
