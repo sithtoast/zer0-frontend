59e37ed22138d847e5995d067da49c71e7bde39e
William Holt <william@williams-mac-studio.local>
2024-04-29 00:56:01 -0400

    turns out that’s a nodejs thing :(

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { execSync } from 'child_process';
 
 const Footer = () => {
-    const gitCommitHash = execSync('git rev-parse HEAD').toString().trim();
 
     return (
         <div style={{ position: 'fixed', right: 0, bottom: 0, padding: '10px', backgroundColor: '#f8f9fa' }}>
-            {`Git Commit: ${gitCommitHash}`}
+            Created with love by sithtoast.
         </div>
     );
 };
