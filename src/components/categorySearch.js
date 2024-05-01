84467c66759ff65b37d53dcdf3e21b8c6ffb44ed
William Holt <william@williams-mac-studio.local>
2024-05-01 04:39:00 -0400

    trying hard with css and failing

--- src/components/categorySearch.js
+++ src/components/categorySearch.js
@@ -22,9 +22,12 @@ const [error, setError] = useState('');
 const [searchQuery, setSearchQuery] = useState('');
 const [errorMessage, setErrorMessage] = useState(null);
 const [currentPage, setCurrentPage] = useState(1);
-const streamsPerPage = 30;
 const [selectedStream, setSelectedStream] = useState(null);
 const [pages, setPages] = useState(0);  // Initialize total pages
+const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+const [nextCursor] = useState(null);
+const [currentGameName, setCurrentGameName] = useState('');
+const [currentCursor, setCurrentCursor] = useState(null);
 
 
 useEffect(() => {
@@ -132,29 +135,27 @@ const toggleFavorite = async (category, e) => {
 		}
 	};
 
-const handleCategorySelect = async (categoryId) => {
-    setSelectedCategory(categoryId);
+const fetchStreams = async (categoryId, cursor) => {
     setLoading(true);
     try {
         const token = localStorage.getItem('token');
         const decoded = jwtDecode(token);
         const userId = decoded.user.userId;
-
+        
         const userProfileResponse = await axios.get(`${apiUrl}/api/users/profile/${userId}`, {
             headers: { 'Authorization': `Bearer ${token}` }
         });
         const twitchAccessToken = userProfileResponse.data.twitch.accessToken;
-
+        
         const response = await axios.get(`${apiUrl}/api/twitch/streams/${categoryId}`, {
             headers: {
                 'Authorization': `Bearer ${twitchAccessToken}`
             },
             params: {
                 first: 1500,  // Fetch all streams
-                after: null  // Use cursor for pagination
+                after: cursor  // Use cursor for pagination
             }
         });
-		setStreams([]);
         const filteredStreams = response.data.streams.filter(stream => stream.viewer_count <= 3);
         // Fetch user info in batches of 100
         const batchSize = 100;
@@ -210,134 +211,159 @@ const handleCategorySelect = async (categoryId) => {
         console.log("Streams fetched:", streams);
     }
 };
+    
+const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    fetchStreams(selectedCategoryId, nextCursor);  // Use the cursor for the next page
+};
+
+const handleClickCategory = (categoryId) => {
+	setCurrentPage(1);  // Reset to first page on category change
+	setStreams([]);  // Clear existing streams
+	setCurrentCursor(null);  // Reset the cursor
+	setSelectedCategoryId(categoryId);  // Set the selected category ID
+	fetchStreams(categoryId, null);  // Fetch without a cursor
+	setCurrentGameName(categories.find(category => category.id === categoryId)?.name);  // Update the game name
+};
 
 return (
     <div>
         <Navbar />
-        <div className="container">
-            {error ? (
-                <div>{error}</div>
-            ) : (
-                <>
-                    <div className="search-results">
-                        <h1>Search Categories</h1>
-                        {errorMessage && <div className="error-message">{errorMessage}</div>}
-                        <form onSubmit={handleSearch}>
-                        <input
-                            type="text"
-                            value={searchQuery}
-                            onChange={(e) => {
-                                setSearchQuery(e.target.value);
-                                handleSearch(e);
-                            }}
-                            placeholder="Search categories"
-                            autoComplete="off"
-                        />
-                        </form>
-                        <table className="category-table">
-                            <tbody>
-                                {categories.map(category => (
-                                    <tr key={category.id} onClick={() => handleCategorySelect(category.id, null)}>
-                                        <td><img src={category.boxArtUrl} alt={`Box art for ${category.name}`} className="category-box-art" /></td>
-                                        <td className="category-name">{category.name}</td>
-                                        <td>
-                                            <button onClick={(e) => toggleFavorite(category, e)} className="favorite-button">
-                                                {favorites.has(category.id) ? '★' : '☆'}
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-					{selectedCategory && (
-						<div className="stream-details">
-							<h2>Streams for {categories.find(cat => cat.id === selectedCategory)?.name || 'selected category'}</h2>
-							{loading ? (
-								[...Array(30)].map((_, i) => (
-									<div key={i} className="col-md-3 mb-4">
-										<div className="card loading-card" aria-hidden="true">
-											<div className="card-body">
-												<h5 className="card-title">
-													<span className="placeholder col-7"></span>
-												</h5>
-												<div className="placeholder-glow">
-													<span className="placeholder col-7"></span>
-													<span className="placeholder col-4"></span>
-													<span className="placeholder col-6"></span>
-													<span className="placeholder col-8"></span>
-												</div>
-											</div>
-										</div>
-									</div>
-								))
-							) : streams.length > 0 ? (
-								<div className="streams-list row">
-								{streams.map((stream) => (
-									<div 
-										key={stream.id} 
-										className={`col-md-4 mb-4 selected-stream ${selectedStream === stream.id ? 'selected-stream' : ''}`}
-										onClick={() => setSelectedStream(stream.user_name)}
-									>
-										<div className="card">
-											<img src={stream.thumbnail_url.replace('{width}x{height}', '320x180')} className="card-img-top" alt="Stream thumbnail" />
-											<div className="card-body">
-												<OverlayTrigger
-													placement="left"
-													overlay={
-														<Tooltip id={`tooltip-${stream.user_name}`} className="large-tooltip">
-															<img src={stream.user_info.profile_image_url} alt={`${stream.user_name}'s profile`} className="small-image" /><br />
-															<strong>{stream.user_name}</strong><br />
-															Status: {stream.user_info.broadcaster_type === '' ? 'Regular User' : stream.user_info.broadcaster_type === 'affiliate' ? 'Affiliate' : stream.user_info.broadcaster_type}<br />
-															Followers: {stream.followerCount}<br />
-															Created at: {new Date(stream.user_info.created_at).toLocaleString()}
-														</Tooltip>
-													}
-												>
-													<h5 className="card-title">
-														{stream.user_name}
-														{stream.user_info.broadcaster_type === "affiliate" && 
-															<img className="affiliate-icon" src={AffiliateIcon} alt="Affiliate" style={{ width: 25, height: 20 }} />
-														}
-													</h5>
-												</OverlayTrigger>
-												<p className="card-text">Viewers: {stream.viewer_count}</p>
-												<p className="card-text">Language: {stream.language}</p>
-												<p className="card-text">Started at: {new Date(stream.started_at).toLocaleString()}</p>
-												<StreamerBadge stream={stream} />
-											</div>
-										</div>
-									</div>
-								))}
-							</div>
-							) : (
-								<p>No streams available for this category.</p>
-							)}
-						</div>
-					)}
-					{pages > 1 && (
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-4 categories">
+                    {error ? (
+                        <div>{error}</div>
+                    ) : (
+                        <>
+                            <div className="search-results">
+                                <h1>Search Categories</h1>
+                                {errorMessage && <div className="error-message">{errorMessage}</div>}
+                                <form onSubmit={handleSearch}>
+                                    <input
+                                        type="text"
+                                        value={searchQuery}
+                                        onChange={(e) => {
+                                            setSearchQuery(e.target.value);
+                                            handleSearch(e);
+                                        }}
+                                        placeholder="Search categories"
+                                        autoComplete="off"
+                                    />
+                                </form>
+                                <table className="category-table">
+                                    <tbody>
+                                        {categories.map(category => (
+                                            <tr key={category.id} onClick={() => handleClickCategory(category.id)}>
+                                                <td><img src={category.boxArtUrl} alt={`Box art for ${category.name}`} className="category-box-art" /></td>
+                                                <td className="category-name">{category.name}</td>
+                                                <td>
+                                                    <button onClick={(e) => toggleFavorite(category, e)} className="favorite-button">
+                                                        {favorites.has(category.id) ? '★' : '☆'}
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </>
+                    )}
+                </div>
+                {selectedCategory && (
+                    <div className="col-md-8 streams">
+                        <h2 className="stream-details">Streams for {categories.find(cat => cat.id === selectedCategory)?.name || 'selected category'}</h2>
+                        <div id="twitch-embed"></div>
+                        <div className="row">
+                            {loading ? (
+                                [...Array(30)].map((_, i) => (
+                                    <div key={i} className="col-md-4 mb-4">
+                                        <div className="card loading-card" aria-hidden="true">
+                                            <div className="card-body">
+                                                <h5 className="card-title">
+                                                    <span className="placeholder col-7"></span>
+                                                </h5>
+                                                <div className="placeholder-glow">
+                                                    <span className="placeholder col-7"></span>
+                                                    <span className="placeholder col-4"></span>
+                                                    <span className="placeholder col-6"></span>
+                                                    <span className="placeholder col-8"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : streams.length ? streams.map(stream => (
+                                <div 
+                                    key={stream.id} 
+                                    className={`col-md-4 mb-4 selected-stream ${selectedStream === stream.id ? 'selected-stream' : ''}`}
+                                    onClick={() => setSelectedStream(stream.user_name)}
+                                >
+                                    <div className="card">
+                                        <img src={stream.thumbnail_url.replace('{width}x{height}', '320x180')} className="card-img-top" alt="Stream thumbnail" />
+                                        <div className="card-body">
+                                            <OverlayTrigger
+                                                placement="left"
+                                                overlay={
+                                                    <Tooltip id={`tooltip-${stream.user_name}`} className="large-tooltip">
+                                                        <img src={stream.user_info.profile_image_url} alt={`${stream.user_name}'s profile`} className="small-image" /><br />
+                                                        <strong>{stream.user_name}</strong><br />
+                                                        Status: {stream.user_info.broadcaster_type === '' ? 'Regular User' : stream.user_info.broadcaster_type === 'affiliate' ? 'Affiliate' : stream.user_info.broadcaster_type}<br />
+                                                        Followers: {stream.followerCount}<br />
+                                                        Created at: {new Date(stream.user_info.created_at).toLocaleString()}
+                                                    </Tooltip>
+                                                }
+                                            >
+                                                <h5 className="card-title">
+                                                    {stream.user_name}
+                                                    {stream.user_info.broadcaster_type === "affiliate" && 
+                                                        <img className="affiliate-icon" src={AffiliateIcon} alt="Affiliate" style={{ width: 25, height: 20 }} />
+                                                    }
+                                                </h5>
+                                            </OverlayTrigger>
+                                            <p className="card-text">Viewers: {stream.viewer_count}</p>
+                                            <p className="card-text">Language: {stream.language}</p>
+                                            <p className="card-text">Started at: {new Date(stream.started_at).toLocaleString()}</p>
+                                            <StreamerBadge stream={stream} />
+                                        </div>
+                                    </div>
+                                </div>
+                            )) : (
+                                <p>No streams available for this category.</p>
+                            )}
+                        </div>
 						<div className="pagination">
-							<button
-								onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-								disabled={currentPage === 1}
-							>
-								Previous
-							</button>
-							<span>Page {currentPage} of {pages}</span>
-							<button
-								onClick={() => setCurrentPage(prev => Math.min(prev + 1, pages))}
-								disabled={currentPage === pages}
-							>
-								Next
-							</button>
-						</div>
-					)}
-                </>
-            )}
+                    <button 
+                        onClick={() => handlePageChange(currentPage - 1)}
+                        disabled={currentPage === 1}
+                    >
+                        Previous
+                    </button>
+                    {[...Array(pages).keys()].slice(Math.max(0, currentPage - 3), currentPage + 2).map(i =>
+                        <button 
+                            key={i} 
+                            onClick={() => handlePageChange(i + 1)}
+                            className={`page-item ${currentPage === (i + 1) ? 'current-page' : ''}`}
+                        >
+                            {i + 1}
+                        </button>
+                    )}
+                    <button 
+                        onClick={() => handlePageChange(currentPage + 1)}
+                        disabled={currentPage === pages}
+                    >
+                        Next
+                    </button>
+                </div>
+                    </div>
+                )}
+            </div>
         </div>
-        <Footer />
+		<Footer />
     </div>
 );
+
+
 
 	};
 
